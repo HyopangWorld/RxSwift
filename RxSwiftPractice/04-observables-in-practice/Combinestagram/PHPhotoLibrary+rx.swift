aa94342e1e57d873282f39dbb2fe6c744f35d726
Hyowon Kim <hyopangworld@gmail.com>
2019-10-25 15:16:56 +0900

    Ch6 Filtering Operator Practice Update Combinestagram project

---
+++ RxSwiftPractice/04-observables-in-practice/Combinestagram/PHPhotoLibrary+rx.swift
@@ -0,0 +1,9 @@
+//
+//  PHPhotoLibrary+rx.swift
+//  Combinestagram
+//
+//  Created by 김효원 on 2019/10/25.
+//  Copyright © 2019 Underplot ltd. All rights reserved.
+//
+
+import Foundation
---
+++ RxSwiftPractice/Combinestagram/Combinestagram/PhotosViewController.swift
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2016-present Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import Photos
+
+import RxSwift
+
+class PhotosViewController: UICollectionViewController {
+    
+    // MARK: public properties
+    
+    // MARK: private properties
+    private lazy var photos = PhotosViewController.loadPhotos()
+    private lazy var imageManager = PHCachingImageManager()
+    
+    private lazy var thumbnailSize: CGSize = {
+        let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
+        return CGSize(width: cellSize.width * UIScreen.main.scale,
+                      height: cellSize.height * UIScreen.main.scale)
+    }()
+    
+    static func loadPhotos() -> PHFetchResult<PHAsset> {
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        return PHAsset.fetchAssets(with: allPhotosOptions)
+    }
+    
+    // public하게 접근 허용하긴 싫다. 다른 클래스에서 onNext(_)를 호출하여 이 subject가 값을 방출하도록 하면 안되니까. (최소한 이 예제에선)
+    // 선택된 사진을 방출할 subject
+    private let selectedPhotosSubject = PublishSubject<UIImage>()
+    
+    // subject의 observable을 방출할 selcectedPhotos 프로퍼티
+    // 이 프로퍼티를 구독하는 것이 MainViewController에서 다른 간섭/변경 없이 사진 sequence를 관찰하는 방법
+    var selectedPhotos: Observable<UIImage> {
+        return selectedPhotosSubject.asObserver()
+    }
+    
+    let bag : DisposeBag = DisposeBag()
+    
+    // MARK: View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let authorized = PHPhotoLibrary.authorized.share()
+        authorized
+            .skipWhile { $0 == false }
+            .take(1) // 한번이라도 true가 나오면 그 뒤는 무시
+            .subscribe(onNext: { [weak self] _ in
+                self?.photos = PhotosViewController.loadPhotos()
+                DispatchQueue.main.async {
+                    self?.collectionView?.reloadData()
+                }
+            })
+        
+        authorized
+            .skip(1)
+            .takeLast(1)
+            .filter { $0 == false }
+            .subscribe(onNext: { [weak self] _ in
+                guard let errorMessage = self?.errorMessage else { return }
+                
+                DispatchQueue.main.async(execute: errorMessage)
+            })
+            .disposed(by: bag)
+    }
+    
+    private func errorMessage(){
+        alert(title: "카메라 접근 제한", text: "You can grant access to Combinestagram from the Settings app")
+            .asObservable()
+            .take(0.5, scheduler: MainScheduler.instance) // 주어진 시간 동안 sequence에서 나온 요소를 가지고 있다가 시간이 지나면 sequence가 complete됨.
+            .subscribe(onCompleted: { [weak self] in
+                self?.dismiss(animated: true, completion: nil)
+                _ = self?.navigationController?.popViewController(animated: true)
+            })
+            .disposed(by: bag)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // selectedPhotosSubject는 MainVC가 끝나야 사라지기 때문에 PhotoVC에서 끝내고 싶으면 Completed 이벤트 생성
+        selectedPhotosSubject.onCompleted()
+    }
+    
+    // MARK: UICollectionView
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let asset = photos.object(at: indexPath.item)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
+        
+        cell.representedAssetIdentifier = asset.localIdentifier
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.imageView.image = image
+            }
+        })
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = photos.object(at: indexPath.item)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell {
+            cell.flash()
+        }
+        
+        imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
+            guard let image = image, let info = info else { return }
+            
+            if let isThumbnail = info[PHImageResultIsDegradedKey as NSString] as? Bool, !isThumbnail {
+                self?.selectedPhotosSubject.onNext(image)
+            }
+            
+        })
+    }
+}
