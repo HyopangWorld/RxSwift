c1cfd144851e4f1102f979ae9d067d28df3cf9c1
HyowonKim <hyopangworld@gmail.com>
2019-10-31 17:52:34 +0900

    Ch13 Intermediate RxCocoa - 도시명 검색 시 indicator 추가

--- RxSwiftPractice/Wundercast/Wundercast/ViewController.swift
+++ RxSwiftPractice/Wundercast/Wundercast/ViewController.swift
@@ -32,37 +32,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var iconLabel: UILabel!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var tempSwitch: UISwitch!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     let bag = DisposeBag()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         style()
-            
-        // 기본 데이터
-        ApiController.shared.currentWeather(city: "RxSwift")
-            .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { data in
-                self.tempLabel.text = "\(data.temperature)°C"
-                self.iconLabel.text = data.icon
-                self.humidityLabel.text = "\(data.humidity)%"
-                self.cityNameLabel.text = data.cityName
-            }).disposed(by: bag)
         
-        
+        // 검색 입력창에 입력이 끝나면 검색이 시작하도록 contolEvent 추가한다.
         let searchText = searchCityName.rx.controlEvent(.editingDidEndOnExit).asObservable()
         let temperature = tempSwitch.rx.controlEvent(.valueChanged).asObservable()
         
         // 도시 검색창에 옵져버 걸기
-        // 검색 입력창에 입력이 끝나면 검색이 시작하도록 contolEvent 추가한다.
         // => 이렇게 하면 빈칸이 넘어올 수 없음
         let search = Observable.from([searchText, temperature])
             .merge()
             .map { self.searchCityName.text }
-            // 빈 텍스트 거르고
-//            .filter { ($0 ?? "").count > 0 }
-            // 텍스트로 ApiController 호출해서 검색
-            // API에서 받은 에러 때문에(observable 에러가 아닌) observable이 dispose 되는 것을 막기 위해 catchError
+            //  텍스트로 ApiController 호출해서 검색 API에서 받은 에러 때문에(observable 에러가 아닌) observable이 dispose 되는 것을 막기 위해 catchError
             .flatMapLatest { text in
                 // 유저가 search를 탭했을 때만 요청을 하므로 catchErrorJustReturn을 거를 수 있다.
                 return ApiController.shared.currentWeather(city: text ?? "Error")
@@ -72,37 +61,43 @@ class ViewController: UIViewController {
             // : 에러를 방출하지 않는 특별한 observable이다. 모든 과정은 UI 변경이 background 쓰레드에서 이뤄지는 것을 방지하기 위해 메인 쓰레드에서 이뤄진다.
             //.  observable이 에러를 방출할 때 어떻게 할 것인지 기본값을 정의하고 있다. 그러므로 driver는 스스로 방출된 에러를 떼어내는게 가능하다.
             .asDriver(onErrorJustReturn: ApiController.Weather.empty)
+
         
+        // MARK: - 검색 결과 나올때까지 indicator 실행
+        // 검색 입력과 검색 결과 모두를 합쳐서 running에
+        let running = Observable.from([
+                searchText.map { _ in true },
+                search.map { _ in false }.asObservable()
+            ])
+            .merge()
+            //  앱이 시작할 때 모든 label을 수동적으로 숨길 필요가 없게 해주는
+            .startWith(true)
+            .asDriver(onErrorJustReturn: false)
+        
+        running.skip(1).drive(activityIndicator.rx.isAnimating).disposed(by: bag)
+        
+        running.drive(tempLabel.rx.isHidden).disposed(by: bag)
+        running.drive(humidityLabel.rx.isHidden).disposed(by: bag)
+        running.drive(cityNameLabel.rx.isHidden).disposed(by: bag)
+        running.drive(iconLabel.rx.isHidden).disposed(by: bag)
+        
+        
+        // MARK: - 검색 결과 혹은 온도 스위치 바뀌었을때 라벨 변경
         // - bind는 subscribe와 비슷한데 맞춤형 producer만 값을 보내고 recevier는 값을 받기만 한다.
         //   producer가 만들어 내는 값에 따라 recevier를 data binding 해주는 것이다.
         //   여기서 search Observer가 producer이고, humidityLabel이 recevier이다
-        
         // - driver 에서 bind(to:) 처럼 행동하는 drive()
         search
             .map {
-                if self.tempSwitch.isOn {
-                    return "\(Int(Double($0.temperature) * 1.8 + 32))°F"
-                }
-                else {
-                    return "\($0.temperature)°C"
-                }
+                if self.tempSwitch.isOn { return "\(Int(Double($0.temperature) * 1.8 + 32))°F" }
+                else { return "\($0.temperature)°C" }
             }
             .drive(tempLabel.rx.text)
             .disposed(by: bag)
 
-        search.map { "\($0.humidity)%" }
-            .drive(humidityLabel.rx.text)
-            .disposed(by: bag)
-
-        search.map { "\($0.cityName)" }
-            .drive(cityNameLabel.rx.text)
-            .disposed(by: bag)
-
-        search.map { "\($0.icon)" }
-            .drive(iconLabel.rx.text)
-            .disposed(by: bag)
-        
-
+        search.map { "\($0.humidity)%" }.drive(humidityLabel.rx.text).disposed(by: bag)
+        search.map { "\($0.cityName)" }.drive(cityNameLabel.rx.text).disposed(by: bag)
+        search.map { "\($0.icon)" }.drive(iconLabel.rx.text).disposed(by: bag)
     }
     
     override func viewDidAppear(_ animated: Bool) {
